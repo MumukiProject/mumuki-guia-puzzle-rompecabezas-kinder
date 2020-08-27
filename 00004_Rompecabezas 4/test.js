cb2b4e13fa14201c39b91b4c0bd04b260613bc14
rocio@mumuki.org <rocio@mumuki.org>
2020-08-27 14:37:48 +0000

    Mumuki Export on 2020-08-27 14:37:48 +0000

--- 00004_Rompecabezas 4/test.js
+++ 00004_Rompecabezas 4/test.js
@@ -2,4 +2,4 @@
 Muzzle.canvasWidth = 600;
 Muzzle.pieceSize = 80;*/
 
-Muzzle.basic(2, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle_1598537743784.jpg')
+Muzzle.basic(2, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle2_1598539043835.png')
