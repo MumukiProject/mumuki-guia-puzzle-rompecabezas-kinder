4a44cdfabcfc9215465ead40d15c0893f0101f35
rocio@mumuki.org <rocio@mumuki.org>
2020-08-27 16:25:07 +0000

    Mumuki Export on 2020-08-27 16:25:07 +0000

---
+++ 00009_Rompecabezas 8/final_state.md
@@ -0,0 +1,1 @@
+<img src='https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle2_1598539043835.png'>
---
+++ 00009_Rompecabezas 8/test.js
@@ -0,0 +1,1 @@
+Muzzle.basic(2, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle2_1598539043835.png')
