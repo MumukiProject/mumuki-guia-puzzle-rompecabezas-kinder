0b5a7998a8213f7d0c22a985d28282179d4667fa
rocio@mumuki.org <rocio@mumuki.org>
2020-08-31 21:05:16 +0000

    Mumuki Export on 2020-08-31 21:05:16 +0000

--- 00001_Rompecabezas 1/final_state.md
+++ 00001_Rompecabezas 1/final_state.md
@@ -1,1 +1,1 @@
-<img src='https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle_1598537743784.jpg'>
+<img src='https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/canasta_llena-01_1598907902250.png'>
--- 00001_Rompecabezas 1/test.js
+++ 00001_Rompecabezas 1/test.js
@@ -1,3 +1,3 @@
 //la pieza de abajo fija
 
-Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle_1598537743784.jpg')
+Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/canasta_llena-01_1598907902250.png')
