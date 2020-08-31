3167d9cacd3c522f8489b02610d14046dfc8728e
rocio@mumuki.org <rocio@mumuki.org>
2020-08-31 21:10:59 +0000

    Mumuki Export on 2020-08-31 21:10:59 +0000

--- 00005_Rompecabezas 5/test.js
+++ 00005_Rompecabezas 5/test.js
@@ -1,1 +1,1 @@
-Muzzle.basic(2, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/prueba_puzzle4_1598539538829.jpg')
+Muzzle.basic(2, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/pulpo_algas-01_1598908246786.png')
