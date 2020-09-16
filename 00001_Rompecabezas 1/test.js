7de8a090d6d0d62ea49a43190fc3cd37814d7e5e
rocio@mumuki.org <rocio@mumuki.org>
2020-09-16 15:23:34 +0000

    Mumuki Export on 2020-09-16 15:23:34 +0000

--- 00001_Rompecabezas 1/test.js
+++ 00001_Rompecabezas 1/test.js
@@ -1,3 +1,3 @@
 //la pieza de abajo fija
 
-Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/canasta_llena-01_1598907902250.png')
+Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/torta1-01_1600269798453.png')
