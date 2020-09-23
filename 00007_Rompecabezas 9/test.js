20b3b7113118972a29124b1c4f9ff9ac13c53124
rocio@mumuki.org <rocio@mumuki.org>
2020-09-23 14:36:18 +0000

    Mumuki Export on 2020-09-23 14:36:18 +0000

--- 00007_Rompecabezas 9/test.js
+++ 00007_Rompecabezas 9/test.js
@@ -1,2 +1,1 @@
-Muzzle.fitImagesHorizontally = true;
 Muzzle.basic(3, 3, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/rompecabezas_final1b-01_1600871417399.png')
