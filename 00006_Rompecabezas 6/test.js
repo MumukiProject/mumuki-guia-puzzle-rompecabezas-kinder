a5608f9567858f8b3f7bae8be5b1bc06b6cb6096
rocio@mumuki.org <rocio@mumuki.org>
2020-09-24 15:31:51 +0000

    Mumuki Export on 2020-09-24 15:31:51 +0000

--- 00006_Rompecabezas 6/test.js
+++ 00006_Rompecabezas 6/test.js
@@ -1,2 +1,2 @@
-Muzzle.strokeWidth = 20;
+Muzzle.strokeWidth = 10;
 Muzzle.basic(1, 5, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/basquet-01_1600270778414.png')
