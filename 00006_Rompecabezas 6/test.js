4dba48dfb07a6e4aa33a7dea17b1f83b5c880df8
franco@mumuki.org <franco@mumuki.org>
2020-09-28 14:24:08 +0000

    Mumuki Export on 2020-09-28 14:24:08 +0000

--- 00006_Rompecabezas 6/test.js
+++ 00006_Rompecabezas 6/test.js
@@ -1,3 +1,3 @@
 Muzzle.strokeWidth = 20;
-Muzzle.strokeColor = 'blue';
+Muzzle.borderFill = 'blue';
 Muzzle.basic(1, 5, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/basquet-01_1600270778414.png')
