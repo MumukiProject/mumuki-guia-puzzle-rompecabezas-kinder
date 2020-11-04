941c3f4ca4a406030d7aea9c13c33374fda89a1a
rocio@mumuki.org <rocio@mumuki.org>
2020-11-04 21:41:57 +0000

    Mumuki Export on 2020-11-04 21:41:57 +0000

--- 00001_Rompecabezas 1/test.js
+++ 00001_Rompecabezas 1/test.js
@@ -3,6 +3,6 @@
 Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/torta1-01_1600269798453.png').then(canvas => {
           canvas.shuffle(0.4);
           canvas.puzzle.pieces.filter(it => !it.metadata.left).forEach(it => it.translate(400, 0))
-          canvas.puzzle.pieces[0].relocateTo(100, 10)
-          canvas.puzzle.pieces[1].relocateTo(100, 120)
+          canvas.puzzle.pieces[0].relocateTo(50, 10)
+          canvas.puzzle.pieces[1].relocateTo(50, 120)
         });
