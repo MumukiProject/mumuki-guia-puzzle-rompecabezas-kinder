21a997138e55649e44fadd66a80e9472e32213c1
rocio@mumuki.org <rocio@mumuki.org>
2020-11-04 21:13:09 +0000

    Mumuki Export on 2020-11-04 21:13:09 +0000

--- 00001_Rompecabezas 1/test.js
+++ 00001_Rompecabezas 1/test.js
@@ -1,3 +1,8 @@
 //la pieza de abajo fija
 
-Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/torta1-01_1600269798453.png')
+Muzzle.basic(1, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/torta1-01_1600269798453.png').then(canvas => {
+          canvas.shuffle(0.4);
+          canvas.puzzle.pieces.filter(it => !it.metadata.left).forEach(it => it.translate(400, 0))
+          canvas.puzzle.pieces[0].relocateTo(360, 60)
+          canvas.puzzle.pieces[1].relocateTo(360, 200)
+        });
