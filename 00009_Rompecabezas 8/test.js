c7dd668d25ce2d23c450dd226020fbd9016f04b5
rocio@mumuki.org <rocio@mumuki.org>
2020-11-05 18:28:44 +0000

    Mumuki Export on 2020-11-05 18:28:44 +0000

--- 00009_Rompecabezas 8/test.js
+++ 00009_Rompecabezas 8/test.js
@@ -1,1 +1,1 @@
-Muzzle.basic(3, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/basquet-01_1600270778414.png')
+Muzzle.basic(3, 2, 'https://raw.githubusercontent.com/MumukiProject/mumuki-guia-puzzle-rompecabezas-kinder/master/assets/basquet-01_1604600796375.svg')
